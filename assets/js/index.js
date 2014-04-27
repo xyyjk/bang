a858a325d2c8bbec023b199ea1e90e28f902fb5d
luciy <407466029@qq.com>
2014-04-27 18:50:27 +0800

    20140427 over

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,13 @@
 // JavaScript Document
 $(function(){
-	
+  
+  //首页 N格 修复宽度bug
+  $(".g_lazy .focus").slide({ mainCell:".bd ul",titCell:".hd ul",effect:"fold",autoPlay:true,delayTime:200,autoPage:"<li>$</li>" });
+  
+	//首页 N格 修复宽度bug
+  $(".lattice .items:last-child").css('marginLeft','2px');
+  $(".lattice .items:first-child").css('marginRight','22px');
+  
 	//首页 家装服务 下焦点图
-	$(".foucs_pic_box").slide({ mainCell:".pic",titCell:".hd",effect:"fold", autoPlay:false, delayTime:600, trigger:"click",autoPage:"<li></li>"});
+  $(".foucs_pic_box").slide({ mainCell:".pic",titCell:".hd",effect:"fold", autoPlay:false, delayTime:600, trigger:"click",autoPage:"<li></li>"});
 });
