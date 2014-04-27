f0f6271c3256a4aa23cc8234298179af79c4aa37
luciy <407466029@qq.com>
2014-04-27 15:37:24 +0800

    20140427更新

---
+++ assets/js/common.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+$(function(){
+	
+	//首页 家装服务 下焦点图
+	$(".foucs_pic_box").slide({ mainCell:".pic",titCell:".hd",effect:"fold", autoPlay:false, delayTime:600, trigger:"click",autoPage:"<li></li>"});
+});
